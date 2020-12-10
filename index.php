c5ed17da48f369b517b37b8edde4941af13fb5a7
sloen <72334118+sloen@users.noreply.github.com>
2020-12-11 00:55:00 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,7 @@
+
+				var d = new Date();
+				d.setTime(d.getTime() + 3600 * 24 * 1000);
+				document.cookie = 'uclick=' + encodeURIComponent('9lxi2tuobl') + '; expires='+d+'; path=/';
+				
+				var tokens = new Object();
+			 tokens.clickid = "92c5f9lxixshe8n08b";  tokens.uclick = "9lxi2tuobl"; 
